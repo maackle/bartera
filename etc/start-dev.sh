9b58d8d51aa259ddace2456b4ea1cda8669f824f
Michael Dougherty <michael@lv11.co>
2013-06-11 23:06:10 -0700

    script

--- etc/start-dev.sh
+++ etc/start-dev.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 cd ~/var/bartera-dev
+kill `less RUNNING_PID`
 git checkout dev
 git pull
-play start -Dconfig.resource=dev.conf -Dhttp.port=9090
+play -Dconfig.resource=dev.conf -Dhttp.port=9090 clean compile start
